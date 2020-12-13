95dcc02ff9194b012e4dbc79f212b8ec10d7575a
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2020-12-13 10:51:14 +0800

    update time

--- js/time.js
+++ js/time.js
@@ -1,22 +1,22 @@
 
 const daynight = document.getElementById("typeit")
 
-if (new Date().getHours() >= 6 && new Date().getHours() < 12){
+if (new Date().getHours() >= 5 && new Date().getHours() < 12){
   daynight.classList.add('day')
   console.log("day")
 }
 
-if (new Date().getHours() >= 12 && new Date().getHours() < 17){
+if (new Date().getHours() >= 12 && new Date().getHours() < 18){
   daynight.classList.add('afternoon')
   console.log("afternoon")
 }
 
-if (new Date().getHours() >= 17 && new Date().getHours() < 24){
+if (new Date().getHours() >= 18 && new Date().getHours() < 24){
   daynight.classList.add('night')
   console.log("evening")
 }
 
-if (new Date().getHours() >= 1 && new Date().getHours() < 6){
+if (new Date().getHours() >= 24 && new Date().getHours() < 5){
   daynight.classList.add('midnight')
   console.log("midnight")
 }

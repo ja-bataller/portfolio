bd69cf1d0ed9262f5f2c853cd1c3ae58b7bf4dd3
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2020-12-18 23:08:34 +0800

    update portfolio

--- js/time.js
+++ js/time.js
@@ -1,22 +1,22 @@
 
-const daynight = document.getElementById("typeit")
+const time = document.getElementById("typeit")
 
 if (new Date().getHours() >= 5 && new Date().getHours() < 12){
-  daynight.classList.add('day')
+  time.classList.add('day')
   console.log("day")
 }
 
 if (new Date().getHours() >= 12 && new Date().getHours() < 18){
-  daynight.classList.add('afternoon')
+  time.classList.add('afternoon')
   console.log("afternoon")
 }
 
-if (new Date().getHours() >= 18 && new Date().getHours() < 00){
-  daynight.classList.add('night')
+if (new Date().getHours() >= 18 || new Date().getHours() < 00){
+  time.classList.add('night')
   console.log("evening")
 }
 
 if (new Date().getHours() >= 00 && new Date().getHours() < 5){
-  daynight.classList.add('midnight')
+  time.classList.add('midnight')
   console.log("midnight")
 }
